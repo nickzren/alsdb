913e7b174d6be758e398908e109175b5a73791e5
Nick Ren <zhong.ren@hotmail.com>
2019-01-24 15:54:24 -0500

    disabled varsome and upgraded JDBC

--- src/main/java/util/DBManager.java
+++ src/main/java/util/DBManager.java
@@ -1,19 +1,18 @@
 package util;
 
-import global.Data;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.sql.*;
-import java.util.Properties;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.apache.tomcat.jdbc.pool.PoolProperties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author nick
  */
 public class DBManager {
 
-    private static DataSource dataSource;
     private static Connection connection;
     private static Statement statement;
 
@@ -22,40 +21,13 @@ public class DBManager {
     private static String dbPassword;
 
     public static void init() throws Exception {
-        initDataFromSystemConfig();
-
-        if (dataSource == null) {
-            PoolProperties p = new PoolProperties();
-            p.setDriverClassName("com.mysql.jdbc.Driver");
-            p.setUrl(dbUrl);
-            p.setUsername(dbUser);
-            p.setPassword(dbPassword);
-            p.setJmxEnabled(true);
-            p.setTestWhileIdle(false);
-            p.setTestOnBorrow(true);
-            p.setValidationQuery("SELECT 1");
-            p.setTestOnReturn(false);
-            p.setValidationInterval(30000);
-            p.setTimeBetweenEvictionRunsMillis(30000);
-            p.setMaxActive(100);
-            p.setInitialSize(10);
-            p.setMaxWait(10000);
-            p.setRemoveAbandonedTimeout(60);
-            p.setMinEvictableIdleTimeMillis(30000);
-            p.setMinIdle(10);
-            p.setLogAbandoned(true);
-            p.setRemoveAbandoned(true);
-            p.setJdbcInterceptors(
-                    "org.apache.tomcat.jdbc.pool.interceptor.ConnectionState;"
-                    + "org.apache.tomcat.jdbc.pool.interceptor.StatementFinalizer");
-            dataSource = new DataSource();
-            dataSource.setPoolProperties(p);
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-        }
-
         if (connection == null || connection.isClosed()) {
-            connection = dataSource.getConnection();
+            initDataFromSystemConfig();
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    dbUrl + "?"
+                    + "user=" + dbUser
+                    + "&password=" + dbPassword);
             statement = connection.createStatement();
         } else if (statement.isClosed()) {
             statement = connection.createStatement();
@@ -81,8 +53,8 @@ public class DBManager {
     public static ResultSet executeQuery(String sqlQuery) throws SQLException {
         return statement.executeQuery(sqlQuery);
     }
-    
-    public static PreparedStatement prepareStatement(String sqlQuery) throws SQLException{
+
+    public static PreparedStatement prepareStatement(String sqlQuery) throws SQLException {
         return connection.prepareStatement(sqlQuery);
     }
 }
